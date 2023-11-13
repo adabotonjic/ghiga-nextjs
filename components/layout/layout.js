0149d0bad01c0ba561d35d392513ba1f7ed998fc
Ada Botonjic <ada.botonjic@antartika.it>
2023-11-13 13:04:45 +0100

    sitemap corretta

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -18,7 +18,7 @@ export default function Layout({ children }) {
         />
         <meta
           property="og:image"
-          content="https://ghirarfellinicola.it/images/furgone.webp"
+          content="https://www.ghirarfellinicola.it/images/furgone.webp"
         />
         <meta property="og:locale" content="it_IT" />
         <meta name="og:title" content={siteTitle} />
@@ -40,21 +40,7 @@ export default function Layout({ children }) {
       </Head>
 
       <GoogleTagManager gtmId="GTM-NK9CVTXJ" />
-      {/*<Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', 'GTM-NK9CVTXJ');
-        `,
-        }}
-      />
-
-      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NK9CVTXJ"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>*/}
+ 
     <Header />
       <main>{children}</main>
     <Footer />
